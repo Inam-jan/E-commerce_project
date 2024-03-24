b036639da9b1ddb4c6895ea4485643a48090a1f5
inamullah <inamkhan03129892825@gmail.com>
2024-03-24 09:23:42 -0700

    first login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 m-4 col-sm-offset-5">
+            <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                
+                <button type="submit" class="btn btn-primary mt-3">Submit</button>
+              </form>
+        </div>
+    </div>
+</div>
